5b0dc4bbf53d250251ebf9640e8345ef2e2e5296
leocherubim <leo.fcherubini@gmail.com>
2015-07-29 23:24:34 -0400

    Adicionando a terceira aula

---
+++ aula-03/js/app.js
@@ -0,0 +1,7 @@
+var MyApp = angular.module('MyApp', []);
+MyApp.controller('listaController', function($scope) {
+	$scope.tarefas = [
+		{'nome':'ir trabalhar', 'prioridade':5},
+		{'nome':'ir a praia', 'prioridade':1}
+	];
+});
